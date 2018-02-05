cb6cda36e60fba3d02f3c78c042d342a4c853da4
Pawan M <pawanmurthy92@gmail.com>
2018-02-05 01:48:07 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -26,8 +26,8 @@ function accessElementInArray(array, index){
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array){
-  //array.shift();
-  return array.shift();
+  array.shift();
+  return array;
 }
 
 function removeElementFromBeginningOfArray(array){
