6c7188d8a761c3d9969927be3ab78fe1a7ee6bc2
Pawan M <pawanmurthy92@gmail.com>
2018-02-05 01:50:37 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -12,13 +12,13 @@ function destructivelyAddElementToBeginningOfArray(array, element){
 }
 
 function addElementToEndOfArray(array, element){
-  array.push(element);
-  return array;
+  
+  return [...array, element];
 }
 
 function destructivelyAddElementToEndOfArray(array, element){
-  
-  return [...array, element];
+  array.push(element);
+  return array; 
 }
 
 function accessElementInArray(array, index){
